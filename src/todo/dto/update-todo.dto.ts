82f4c2f3ad9e938ea98d7011b3641558e0f299ac
Samuel Arandia <sarandia@advise.cl>
2023-12-08 12:12:47 -0300

    Initial commit + todo dev

---
+++ src/todo/dto/update-todo.dto.ts
@@ -0,0 +1,10 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTodoDto } from './create-todo.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+   
+    @IsBoolean()
+    @IsOptional()
+    done?: boolean;
+}
